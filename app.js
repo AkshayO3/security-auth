14e496700ce064b3f0eea8192b13757796b9e9a0
AkshayO3 <akshaybhandari020@gmail.com>
2023-06-29 00:09:02 +0530

    Level 2:Encryption

--- app.js
+++ app.js
@@ -1,28 +1,31 @@
 const express=require("express")
 const bodyParser=require("body-parser")
 const ejs=require("ejs")
-const mongoose=require("mongoose")
+const mongoose = require("mongoose")
+const encrypt = require("mongoose-encryption")
 
 const app=express()
 
 app.use(express.static("public"))
-app.set('view engine','ejs')
-app.use(bodyParser.urlencoded({extended:true}))
+app.set('view engine', 'ejs')
+app.use(bodyParser.urlencoded({extended: true}))
 
-mongoose.connect("mongodb://localhost:27017/userDB",{useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true, useUnifiedTopology: true})
 
-const userSchema=mongoose.Schema({
-    username:String,
-    password:String
+var userSchema = new mongoose.Schema({
+    username: String,
+    password: String
 })
 
-const User=mongoose.model('User',userSchema)
+const secret = "This is the secret string."
+userSchema.plugin(encrypt, {secret: secret, encryptedFields: ['password']})
+const User = mongoose.model('User', userSchema)
 
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
     res.render("home")
 })
 
-app.get("/login",(req,res)=>{
+app.get("/login", (req, res) => {
     res.render("login")
 })
 
