329c010c01fefafaee93d39769afd9f5692f1636
AkshayO3 <akshaybhandari020@gmail.com>
2023-07-02 22:50:20 +0530

    Final

--- app.js
+++ app.js
@@ -55,7 +55,7 @@ passport.deserializeUser((id, done) => {
 passport.use(new GoogleStrategy({
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: "http://localhost:3000/auth/google/secrets",
+        callbackURL: "https://secrets-fplp.onrender.com/auth/google/secrets",
         userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
     },
     function (accessToken, refreshToken, profile, cb) {
